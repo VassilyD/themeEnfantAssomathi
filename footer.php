eb4216a4aa81d594992c05fc55056d146be5bfcf
Osteria Mathi <vassily.dubois@gmail.com>
2017-09-14 07:24:22 +0000

    premier commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+?>
+
+<!--<img class="footer" src="wp-content/themes/onepress-child/assets/images/footer.png"><?php echo 'toto' ?></img>-->
+<!--<div class="gradient"></div>-->
+<div class="foo"></div>
+
+<svg width="190" height="160">
+  <path d="M10 80 Q 52.5 10, 95 80 T 180 80" stroke="#004D84" fill="#004D84"/>
+</svg>
