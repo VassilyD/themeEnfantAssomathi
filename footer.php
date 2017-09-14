634324a92a2a96ac8d5aa96e7a50ed5aee8e5c00
Osteria Mathi <vassily.dubois@gmail.com>
2017-09-14 12:13:31 +0000

    update footer

--- footer.php
+++ footer.php
@@ -3,8 +3,14 @@
 
 <!--<img class="footer" src="wp-content/themes/onepress-child/assets/images/footer.png"><?php echo 'toto' ?></img>-->
 <!--<div class="gradient"></div>-->
-<div class="foo"></div>
 
-<svg width="190" height="160">
-  <path d="M10 80 Q 52.5 10, 95 80 T 180 80" stroke="#004D84" fill="#004D84"/>
+
+<svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="100%" height="560px" viewBox="0 0 960 560" style="enable-background:new 0 0 960 560;" xml:space="preserve">
+<g>
+	<path style="fill:#25A59F;" d="M937.373,166.873c3.783,0,7.566,0,11.349,0l0.611,220.325L20.853,387l-0.147-142.429
+		c1.775,0.64,3.531,1.338,5.326,1.912c65.327,20.872,132.498,31.095,200.83,33.975c23.08,0.972,46.275,1.166,69.338,0.057
+		c27.977-1.346,55.945-3.792,83.782-6.948c41.038-4.653,81.386-13.272,120.966-25.023c66.66-19.791,133.252-39.76,201.406-53.976
+		c50.37-10.507,101.113-18.587,152.384-22.856C879.934,169.613,935.087,167.161,937.373,166.873z"/>
+</g>
 </svg>
